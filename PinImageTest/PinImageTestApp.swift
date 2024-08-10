8f9f27380053c3c495d0af7197709ace38abb7c8
developer@gakkie.nl <developer@gakkie.nl>
2024-08-10 12:34:21 +0200

    Initial Commit

---
+++ PinImageTest/PinImageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinImageTestApp.swift
+//  PinImageTest
+//
+//  Created by Gakkie Gakkienl on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PinImageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
